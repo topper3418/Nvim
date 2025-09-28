7a6fe5c0f26aaa0349f34ec28dcaa9471434803c
topper3418 <67675579+topper3418@users.noreply.github.com>
2025-09-27 21:56:42 -0500

    misc fixes to the config

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -17,7 +17,7 @@ return { -- Autoformat
 			-- Disable "format_on_save lsp_fallback" for languages that don't
 			-- have a well standardized coding style. You can add additional
 			-- languages here or re-enable it for the disabled ones.
-			local disable_filetypes = { c = true, cpp = true }
+			local disable_filetypes = { c = true, cpp = true, sql = true }
 			return {
 				timeout_ms = 500,
 				lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
@@ -26,13 +26,14 @@ return { -- Autoformat
 		formatters_by_ft = {
 			lua = { "stylua" },
 			-- Conform can also run multiple formatters sequentially
-			python = { "isort", "black" },
+			python = { "black" },
 			--
 			-- You can use a sub-list to tell conform to run *until* a formatter
 			-- is found.
 			javascript = { "prettier" },
 			typescript = { "prettier" },
 			json = { "jq" },
+			sql = { "sql-formatter" },
 		},
 		formatters = {
 			black = {
